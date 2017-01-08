22e03c2acd691d42e676c98eb57d92fb7d196267
Emmet Susslin <esusslin@gmail.com>
2017-01-07 20:59:42 -0800

    hashes and mentions linked and functional

--- this_bores_me/this_bores_me/this-bores-me-Bridging-Header.m
+++
@@ -1,9 +0,0 @@
-//
-//  this-bores-me.m
-//  this_bores_me
-//
-//  Created by Emmet Susslin on 12/13/16.
-//  Copyright © 2016 Emmet Susslin. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
